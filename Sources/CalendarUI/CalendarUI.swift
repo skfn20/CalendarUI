f9311bc10dbce2089b449f2e5bb885cefea7032b
skfn20 <46715655+skfn20@users.noreply.github.com>
2024-09-29 21:13:13 +0900

    Init Swift Package

---
+++ Sources/CalendarUI/CalendarUI.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct CalendarView: View {
+    
+    public var body: some View {
+        Text("say hello")
+    }
+}
